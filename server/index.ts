2104e5fb9988b786d81d8760f690439b32d8d1c0
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2020-05-21 02:39:14 -0700

    added an api router to the server

--- server/index.ts
+++ server/index.ts
@@ -1,5 +1,6 @@
 import { resolve } from 'path';
 
+import bodyParser from 'body-parser';
 import * as chalk from 'chalk';
 import express, {
   NextFunction,
@@ -7,6 +8,7 @@ import express, {
   RequestHandler,
   Response,
 } from 'express';
+import morgan from 'morgan';
 
 const port = process.env.PORT || process.argv[2] || 8080;
 const wrap = (fn: RequestHandler) => (
@@ -15,7 +17,13 @@ const wrap = (fn: RequestHandler) => (
 
 const app = express();
 
+const apiRouter = express.Router();
+
+app.use(bodyParser.json());
+app.use(morgan('dev'));
 app.use(express.static(resolve('public')));
+
+app.use('/api', apiRouter);
 
 app.listen(port, () =>
   console.log(`Server running on port ${chalk.green(port)}`)
