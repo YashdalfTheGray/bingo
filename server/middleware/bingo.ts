e46aefdc1a91a1f6f32c5cce5dcb29e15da999ce
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2020-05-24 10:04:38 -0700

    added some methods to generate bingo columns and an api

--- server/middleware/bingo.ts
+++ server/middleware/bingo.ts
@@ -2,10 +2,33 @@ import { Router } from 'express';
 
 import { generateSingleColumn } from '../utils/generator';
 
+enum ColumnName {
+  B = 1,
+  I,
+  N,
+  G,
+  O,
+}
+
+const generateFirstColumn = (): number[] => generateSingleColumn(1, 15);
+const generateSecondColumn = (): number[] => generateSingleColumn(16, 30);
+const generateThirdColumn = (): number[] =>
+  generateSingleColumn(31, 45).map((v, i) => (i === 2 ? -1 : v));
+const generateFourthColumn = (): number[] => generateSingleColumn(46, 60);
+const generateFifthColumn = (): number[] => generateSingleColumn(61, 75);
+
 const bingoRouter = Router();
 
 bingoRouter.get('/one', (_, res) => {
-  res.json(generateSingleColumn(1, 15));
+  res.json(
+    [
+      generateFirstColumn(),
+      generateSecondColumn(),
+      generateThirdColumn(),
+      generateFourthColumn(),
+      generateFifthColumn(),
+    ].flat()
+  );
 });
 
 export default bingoRouter;
