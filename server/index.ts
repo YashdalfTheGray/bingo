89fcbe2524585361930fb7bba92aa787daf7b9b2
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2020-05-24 10:05:17 -0700

    partial webpack dev server setup, will complete when webpack is set up

--- server/index.ts
+++ server/index.ts
@@ -9,8 +9,17 @@ import express, {
   Response,
 } from 'express';
 import morgan from 'morgan';
+import * as webpack from 'webpack';
+import webpackDevMiddleware from 'webpack-dev-middleware';
+import webpackHotMiddleware from 'webpack-hot-middleware';
+
+import webpackConfig from '../webpack.config';
 
 import bingoRouter from './middleware/bingo';
+
+type WebpackModeType = 'none' | 'development' | 'production';
+const mode: WebpackModeType = process.env.NODE_ENV as WebpackModeType;
+(webpackConfig as webpack.Configuration).mode = mode;
 
 const port = process.env.PORT || process.argv[2] || 8080;
 const wrap = (fn: RequestHandler) => (
