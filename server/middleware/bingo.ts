7fcf337ded9d7be3883131667bd2f48a0ff05716
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2020-05-27 02:29:01 -0700

    /bingo/one now returns a card string instead of numbers

--- server/middleware/bingo.ts
+++ server/middleware/bingo.ts
@@ -1,5 +1,7 @@
 import { Router } from 'express';
 
+import { encodeColumns, hashColumns } from '../../common/cardUtils';
+
 import { generateSingleColumn } from '../utils/generator';
 
 enum ColumnName {
@@ -13,22 +15,24 @@ enum ColumnName {
 const generateFirstColumn = (): number[] => generateSingleColumn(1, 15);
 const generateSecondColumn = (): number[] => generateSingleColumn(16, 30);
 const generateThirdColumn = (): number[] =>
-  generateSingleColumn(31, 45).map((v, i) => (i === 2 ? -1 : v));
+  generateSingleColumn(31, 45).map((v, i) => (i === 2 ? 0 : v));
 const generateFourthColumn = (): number[] => generateSingleColumn(46, 60);
 const generateFifthColumn = (): number[] => generateSingleColumn(61, 75);
 
 const bingoRouter = Router();
 
 bingoRouter.get('/one', (_, res) => {
-  res.json(
-    [
-      generateFirstColumn(),
-      generateSecondColumn(),
-      generateThirdColumn(),
-      generateFourthColumn(),
-      generateFifthColumn(),
-    ].flat()
-  );
+  const columns = [
+    generateFirstColumn(),
+    generateSecondColumn(),
+    generateThirdColumn(),
+    generateFourthColumn(),
+    generateFifthColumn(),
+  ];
+
+  res.json({
+    card: [...encodeColumns(columns), hashColumns(columns)].join('-'),
+  });
 });
 
 export default bingoRouter;
