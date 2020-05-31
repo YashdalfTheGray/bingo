b0445f0cc59b5deb7d059dec3b70604f824f7bfb
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2020-05-31 08:25:42 -0700

    added an index file and wired up the hot reloading

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,12 @@
 import App from './App';
+import './index.scss';
 
-document.querySelector('#app-root').innerHTML = new App().render();
+document.querySelector('#app-root')!.innerHTML = new App().render();
+
+if (module.hot) {
+  module.hot.accept('./App.ts', () => {
+    // tslint:disable-next-line:no-console
+    console.log('New app.ts module detected');
+    document.querySelector('#app-root')!.innerHTML = new App().render();
+  });
+}
