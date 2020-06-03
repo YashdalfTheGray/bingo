0f056dfb333a943762522a04a5ecc2bced4a32c8
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2020-06-03 00:22:32 -0700

    centered content and wired up the generator and card subcomponents

--- client/components/Content/Content.ts
+++ client/components/Content/Content.ts
@@ -1,18 +1,40 @@
 import Component from '@bingo/client/Component';
+import Card from '@bingo/components/Card';
+import Generator from '@bingo/components/Generator';
+
+import './content.scss';
+
+interface IContentState {
+  queryParams: { [k: string]: string };
+}
 
 export default class Content extends Component {
+  private state: IContentState;
   constructor() {
     super('Content');
+
+    this.state = {
+      queryParams: [
+        ...new URLSearchParams(window.location.search).entries(),
+      ].reduce(
+        (acc, [key, value]) => ({
+          ...acc,
+          [key]: value,
+        }),
+        {}
+      ),
+    };
   }
 
   public render() {
+    const {
+      queryParams: { card: card },
+    } = this.state;
+
     return `
-      <div class="content flex-column flex-item-dynamic">
-        <div>this</div>
-        <div>is</div>
-        <div>a</div>
-        <div>test</div>
-      <div>
+      <div class="content flex-column flex-item-expand">
+        ${card ? new Card({ card }).render() : new Generator().render()}
+      </div>
     `;
   }
 }
