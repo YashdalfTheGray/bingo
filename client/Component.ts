d1b4a24cc006039f3636fb0c86084340ea39eab3
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2020-06-04 01:28:47 -0700

    added props and state to the component, with type support and a setState method

--- client/Component.ts
+++ client/Component.ts
@@ -1,3 +1,5 @@
+import merge from 'lodash.merge';
+
 interface IDocumentWithRegistry extends Document {
   componentRegistry: object;
   nextId: number;
@@ -6,16 +8,26 @@ interface IDocumentWithRegistry extends Document {
 (document as IDocumentWithRegistry).componentRegistry = {};
 (document as IDocumentWithRegistry).nextId = 0;
 
-export default class Component {
+export default class Component<P = {}, S = {}> {
   protected id: string;
+  protected state: S;
+  protected props: P;
 
-  constructor(type: string = 'Component') {
+  constructor(type: string = 'Component', props?: P) {
     this.id = `${type}${++(document as IDocumentWithRegistry).nextId}`;
     (document as IDocumentWithRegistry).componentRegistry[this.id] = this;
+
+    if (props) {
+      this.props = props;
+    }
   }
 
   public render(props?: object): string {
     return '';
+  }
+
+  protected setState(newState: Partial<S>) {
+    this.state = merge({}, this.state, newState);
   }
 
   protected callClassFunction(name: string) {
