730f646260f7358522707f823b94e6cdfb775eff
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2020-06-06 04:34:27 -0700

    added a way for generator to generate the bingo cards

--- client/components/Generator/Generator.ts
+++ client/components/Generator/Generator.ts
@@ -1,4 +1,5 @@
 import Component from '@bingo/client/Component';
+import { getOneCard } from '@bingo/client/service';
 
 import './generator.scss';
 
@@ -9,6 +10,41 @@ interface IGeneratorState {
 export default class Generator extends Component<{}, IGeneratorState> {
   constructor() {
     super('Generator');
+
+    this.state = {
+      cards: [],
+    };
+  }
+
+  public async generateCards() {
+    const numberInput = document.querySelector<HTMLInputElement>(
+      '#card-number-input'
+    );
+
+    const cards = await Promise.all(
+      new Array(parseInt(numberInput!.value, 10))
+        .fill(window.location.href)
+        .map(async (l) => {
+          let firstTime = true;
+          let hash = '';
+          let card: string = '';
+          while (firstTime || !this.isUniqueCard(hash)) {
+            if (firstTime) {
+              firstTime = false;
+            }
+            card = await getOneCard();
+            hash = card.split('-')[0];
+          }
+
+          return `${l}?card=${card}`;
+        })
+    );
+
+    this.setState({ cards });
+
+    document.querySelector(
+      '#card-links-container'
+    )!.innerHTML = this.state.cards.map((c) => `<p>${c}</p>`).join('');
   }
 
   public render() {
@@ -19,4 +55,7 @@ export default class Generator extends Component<{}, IGeneratorState> {
       </div>
     `;
   }
+
+  private isUniqueCard = (hash: string) =>
+    !this.state.cards.map((c) => c.split('-')[0]).includes(hash);
 }
