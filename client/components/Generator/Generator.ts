ee325ae5b6c519694b64692abc68a9b25f94bd33
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2020-06-07 11:08:32 -0700

    wired up some input validation for the number of cards

--- client/components/Generator/Generator.ts
+++ client/components/Generator/Generator.ts
@@ -21,6 +21,12 @@ export default class Generator extends Component<{}, IGeneratorState> {
       '#card-number-input'
     );
 
+    const numberOfCards = parseInt(numberInput!.value, 10);
+
+    if (!this.validateCardNumber(numberOfCards)) {
+      return;
+    }
+
     const cards = await Promise.all(
       new Array(parseInt(numberInput!.value, 10))
         .fill(window.location.href)
@@ -47,13 +53,30 @@ export default class Generator extends Component<{}, IGeneratorState> {
     )!.innerHTML = this.state.cards.map((c) => `<p>${c}</p>`).join('');
   }
 
+  public handleInputChanged() {
+    const numberInput = document.querySelector<HTMLInputElement>(
+      '#card-number-input'
+    );
+    const value = parseInt(numberInput!.value, 10);
+    if (!this.validateCardNumber(value)) {
+      numberInput?.classList.add('validation-error');
+    } else {
+      numberInput?.classList.remove('validation-error');
+    }
+  }
+
   public render() {
     return `
       <div class="generator flex-column">
         <div class="flex-row">
-          <input id="card-number-input" type="number" placeholder="A number, e.g. 5"/>
+          <input
+            id="card-number-input"
+            type="number"
+            placeholder="A number, e.g. 5"
+            onChange="${this.callClassFunction('handleInputChanged')}"/>
           <button
             id="generate-button"
+            class="button"
             onClick="${this.callClassFunction('generateCards')}">
             Generate
           </button>
@@ -67,4 +90,6 @@ export default class Generator extends Component<{}, IGeneratorState> {
 
   private isUniqueCard = (hash: string) =>
     !this.state.cards.map((c) => c.split('-')[0]).includes(hash);
+
+  private validateCardNumber = (val: number) => !!val && val > 0;
 }
