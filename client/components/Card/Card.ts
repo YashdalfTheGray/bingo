f3260dcc01fe90c299add174b6cbc69c747f02a3
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2020-06-13 11:48:46 -0700

    added clicking and toggling an 'x' on the number to the card

--- client/components/Card/Card.ts
+++ client/components/Card/Card.ts
@@ -2,6 +2,8 @@ import Component from '@bingo/client/Component';
 
 import { decodeColumns, hashColumns } from '@bingo/common/cardUtils';
 
+import * as close from '@bingo/client/icons/close.svg';
+
 import './card.scss';
 
 interface ICardProps {
@@ -11,6 +13,19 @@ interface ICardProps {
 export default class Card extends Component<ICardProps> {
   constructor(props: ICardProps) {
     super('Card', props);
+  }
+
+  public handleNumberClick(row: number, column: number) {
+    const visibleClassName = 'visible';
+    const cross = document.querySelector(
+      `.number-container[data-row="${row}"][data-column="${column}"] > .number-cross`
+    );
+
+    if (cross?.classList.contains(visibleClassName)) {
+      cross.classList.remove(visibleClassName);
+    } else {
+      cross?.classList.add(visibleClassName);
+    }
   }
 
   public render() {
@@ -26,15 +41,23 @@ export default class Card extends Component<ICardProps> {
       return `
         <div class="card grid-container">
           ${colsToDisplay
-            .map((c, i) =>
+            .map((c, col) =>
               c
                 .map(
-                  (e, j) =>
+                  (e, row) =>
                     `<span
                       class="number-container"
-                      data-grid-order="${i + j}"
-                      data-row="${j + 1}"
-                      data-column="${i + 1}">${e === 0 ? '' : e}</span>`
+                      data-grid-order="${col + row}"
+                      data-row="${row + 1}"
+                      data-column="${col + 1}"
+                      onClick="${this.callClassFunction(
+                        'handleNumberClick',
+                        row + 1,
+                        col + 1
+                      )}">
+                        <span>${e === 0 ? '' : e}</span>
+                        <span class="number-cross">${close}</span>
+                      </span>`
                 )
                 .join('\n')
             )
