4b681ece0d45f831f925c3f791658c355e881f37
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2020-07-10 15:17:43 -0700

    added a parent container for the chart

--- docs/utils.ts
+++ docs/utils.ts
@@ -3,5 +3,28 @@ import * as jsdom from 'jsdom';
 export function buildChart(data: { [key: string]: number }): string {
   const chart = new jsdom.JSDOM('<!DOCTYPE html>');
 
+  chart.window.document.body.appendChild(
+    buildParent(chart, Object.keys(data).length)
+  );
+
   return chart.serialize();
+}
+
+function buildParent(dom: jsdom.JSDOM, datapoints: number): HTMLElement {
+  const parentStyles = [
+    'display: grid;',
+    `grid-template-columns: repeat(${datapoints}, auto);`,
+    'justify-content: start;',
+    'grid-column-gap: 10px;',
+    'border-left: solid 1px rgba(0,0,0,0.5);',
+    'border-bottom: solid 1px rgba(0,0,0,0.5);',
+    'padding-left: 10px;',
+    `width: ${60 * datapoints}px;`,
+  ].join(' ');
+
+  const parent = dom.window.document.createElement('div');
+  parent.id = 'chart-parent';
+  parent.style.cssText = parentStyles;
+
+  return parent;
 }
