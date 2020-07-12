418c5ae8fa6be00917c2a0c1048291a737f3feba
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2020-07-12 01:41:00 -0700

    moved charting and display logic to utils

--- docs/workbook.ts
+++ docs/workbook.ts
@@ -1,12 +1,3 @@
-import * as fs from 'fs';
-import { promisify } from 'util';
-
-const writeFileAsync = promisify(fs.writeFile);
-
-import * as jsdom from 'jsdom';
-
-const chart = new jsdom.JSDOM('<!DOCTYPE html>');
-
 const minRange = 1;
 const maxRange = 10;
 
@@ -21,28 +12,6 @@ for (let i = 0; i < 100000; i++) {
   results[num] += 1;
 }
 
-const resultsCode = chart.window.document.createElement('pre');
-resultsCode.innerHTML = JSON.stringify(
-  results.reduce(
-    (acc, v, i) => ({
-      ...acc,
-      [i]: v,
-    }),
-    {}
-  ),
-  null,
-  2
-);
-chart.window.document.body.appendChild(resultsCode);
-
 // tslint:disable no-console
 console.log(results);
 // tslint:enable no-console
-
-(async () => {
-  try {
-    writeFileAsync('docs/output.html', chart.serialize(), 'utf-8');
-  } catch (err) {
-    console.error(err); // tslint:disable-line no-console
-  }
-})();
