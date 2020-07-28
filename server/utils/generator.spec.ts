944940fcdcc399e186db954ce0a5d522b5f18bbb
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2020-07-28 01:29:35 -0700

    added a test that takes advantage of the custom error

--- server/utils/generator.spec.ts
+++ server/utils/generator.spec.ts
@@ -7,6 +7,8 @@ import {
   generateSingleColumnBoundsExclusive,
 } from './generator';
 
+import { ValidationError } from './validation';
+
 test('dedupe removes duplicates from array', (t) => {
   const arr = [1, 2, 3, 4, 4, 4];
   t.deepEqual([1, 2, 3, 4], dedupe(arr));
@@ -23,6 +25,14 @@ test('generateArrayOfNumbers generates within constraints', (t) => {
   nums.forEach((n) => {
     t.assert(n >= 1 && n <= 15, 'numbers are not within the specified range');
   });
+});
+
+test('generateArrayOfNumbers throws when there are too few unique numbers in range', (t) => {
+  const error = t.throws<ValidationError<number>>(() =>
+    generateArrayOfNumbers(10, 1, 5)
+  );
+  t.assert(error instanceof ValidationError);
+  t.is(error.argumentName, 'length');
 });
 
 test('generateSingleColumn generates 5 unique numbers in range', (t) => {
