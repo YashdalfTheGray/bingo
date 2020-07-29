071c66e0f42af36a0c62a99c70ae0625969548c1
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2020-07-29 01:12:36 -0700

    added some tests for validation error and properties

--- server/utils/validation.spec.ts
+++ server/utils/validation.spec.ts
@@ -9,3 +9,15 @@ test('assert does not throw for a condition that is true', (t) => {
 test('assert throws for a condition that is false', (t) => {
   t.throws(() => validation.assert(false, 'test message'));
 });
+
+test('ValidationError has a property for arg name and value', (t) => {
+  const err = new validation.ValidationError('test', 2);
+  t.is(err.argumentName, 'test');
+  t.is(err.value, 2);
+});
+
+test('ValidationError properties are read only', (t) => {
+  const err = new validation.ValidationError('test', 2);
+  const thrownErr = t.throws(() => ((err as any).argumentName = 'test'));
+  t.assert(thrownErr instanceof TypeError);
+});
