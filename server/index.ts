ee2b60b307f8251ebd42beb462cd22742e9f45cc
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2020-11-24 05:07:02 -0500

    fixed a bug in the async await wrapper function

--- server/index.ts
+++ server/index.ts
@@ -16,7 +16,7 @@ import hotModuleReloadingSetup from './utils/hmr';
 const port = process.env.PORT || process.argv[2] || 8080;
 const wrap = (fn: RequestHandler) => (
   ...args: [Request, Response, NextFunction]
-) => fn(...args).catch(args[2]);
+) => Promise.resolve(fn(...args)).catch(args[2]);
 
 const app = express();
 
