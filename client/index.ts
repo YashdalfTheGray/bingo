4f7247111605ce325514a3e61c8a9cd770021fca
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2021-11-25 16:18:14 -0500

    use some hardcoded for now event bubbling to handle interactivity

--- client/index.ts
+++ client/index.ts
@@ -1,12 +1,34 @@
 import App from '@bingo/client/App';
+import { IDocumentWithRegistry } from './Component';
+import Generator from './components/Generator';
 import './index.scss';
 
-document.querySelector('#app-root')!.innerHTML = new App().render();
+const rootApp = document.querySelector('#app-root')!;
+
+rootApp.addEventListener('click', (e) => {
+  if ((e.target as HTMLElement).id === 'generate-button') {
+    (
+      (document as IDocumentWithRegistry).componentRegistry
+        .Generator4 as Generator
+    ).generateCards();
+  }
+});
+
+rootApp.addEventListener('change', (e) => {
+  if ((e.target as HTMLElement).id === 'card-number-input') {
+    (
+      (document as IDocumentWithRegistry).componentRegistry
+        .Generator4 as Generator
+    ).handleInputChanged();
+  }
+});
+
+rootApp.innerHTML = new App().render();
 
 if (process.env.NODE_ENV === 'development' && module.hot) {
   module.hot.accept('./App.ts', () => {
     // tslint:disable-next-line:no-console
     console.log('New app.ts module detected');
-    document.querySelector('#app-root')!.innerHTML = new App().render();
+    rootApp.innerHTML = new App().render();
   });
 }
