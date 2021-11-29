293331338c2b42c22a7b751803c8f496a9dcce73
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2021-11-28 19:04:01 -0500

    added data- attributes to the expected things

--- client/components/CardDetailRow/CardDetailRow.ts
+++ client/components/CardDetailRow/CardDetailRow.ts
@@ -32,7 +32,7 @@ export default class CardDetailRow extends Component<ICardDetailRowProps> {
     const { card } = this.props;
 
     return `
-    <div class="flex-row card-detail-row">
+    <div class="flex-row card-detail-row" data-card-hash="${card.hash}">
       <span class="card-number">
         <a href="${card.link}" target="_blank">
           Card #${parseInt(card.hash, 16).toString(10)}
@@ -43,12 +43,7 @@ export default class CardDetailRow extends Component<ICardDetailRowProps> {
           class="icon button copy-button"
           data-card-hash="${card.hash}"
           data-card-link="${card.link}"
-          data-row-id="${this.id}"
-          onClick="${this.callClassFunction(
-            'handleCardCopy',
-            card.link,
-            card.hash
-          )}">
+          data-row-id="${this.id}">
           ${share}
         </button>
         <span class="icon done-icon">${done}</span>
