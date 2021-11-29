9c6e2d7d191b811616124e25e5133a8c88e6d721
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2021-11-29 18:46:25 -0500

    added a utility function to determine if number container

--- client/util.ts
+++ client/util.ts
@@ -1,4 +1,4 @@
-const tryToCopyLink = async (link: string) => {
+export const tryToCopyLink = async (link: string) => {
   const input = document.querySelector<HTMLInputElement>(
     '#clipboard-helper-input'
   )!;
@@ -8,11 +8,16 @@ const tryToCopyLink = async (link: string) => {
   return document.execCommand('copy');
 };
 
-const isTargetAnIconButton = (
+export const isTargetAnIconButton = (
   target: EventTarget | null
 ): target is HTMLButtonElement =>
   (target instanceof HTMLButtonElement && target.classList.contains('icon')) ||
   target instanceof SVGPathElement ||
   target instanceof SVGElement;
 
-export { tryToCopyLink, isTargetAnIconButton };
+export const isTargetANumberContainer = (
+  target: EventTarget | null
+): target is HTMLSpanElement =>
+  target instanceof HTMLSpanElement &&
+  target.classList.contains('number-container') &&
+  !target.classList.contains('is-header');
