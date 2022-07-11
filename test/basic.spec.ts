a5eeceeecb84683257766631a4c3af40291f3d2f
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2022-07-11 10:21:35 -0400

    added another test to check for the generate button

--- test/basic.spec.ts
+++ test/basic.spec.ts
@@ -3,11 +3,25 @@ import test from 'ava';
 import { withPageAt } from './utils';
 
 test(
-  'we see something on the page',
+  'something shows up on the page',
   withPageAt('http://localhost:8080'),
   async (t, page) => {
     const header = await page.$('#app-root .bingo-header .title');
 
     t.truthy(header);
+  }
+);
+
+test(
+  'we see the generate view when there is no card query param',
+  withPageAt('http://localhost:8080'),
+  async (t, page) => {
+    const numberInput = await page.$('#app-root .generator #card-number-input');
+    const generateButton = await page.$(
+      '#app-root .generator #generate-button'
+    );
+
+    t.truthy(numberInput);
+    t.truthy(generateButton);
   }
 );
