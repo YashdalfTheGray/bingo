fbd3373b17e385d6658f8648c98ee51debfcb38f
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2022-07-27 22:57:22 -0400

    added a test that checks just one number container, which works

--- test/cardview.spec.ts
+++ test/cardview.spec.ts
@@ -33,6 +33,27 @@ test(
   }
 );
 
+test(
+  'clicking on a number container marks it crossed',
+  withPage,
+  async (t, page) => {
+    const cardId = await getOneBingoCard(getBaseAppUrl());
+    const cardUrl = getUrlForCard(cardId);
+
+    await page.goto(cardUrl);
+
+    await page.click(
+      '#app-root .content .card .number-container:not(.is-header)'
+    );
+
+    const numberCrosses = await page.$$(
+      '#app-root .content .card .number-container:not(.is-header) .number-cross.visible'
+    );
+
+    t.is(numberCrosses.length, 1);
+  }
+);
+
 // test(
 //   'clicking on a number container marks it crossed',
 //   withPage,
