162e133bc93458b2989116a2056e1f42ffbcfa32
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2022-07-28 21:42:42 -0400

    added a test that tries to cross all the numbers

--- test/cardview.spec.ts
+++ test/cardview.spec.ts
@@ -54,25 +54,31 @@ test(
   }
 );
 
-// test(
-//   'clicking on a number container marks it crossed',
-//   withPage,
-//   async (t, page) => {
-//     const cardId = await getOneBingoCard(getBaseAppUrl());
-//     const cardUrl = getUrlForCard(cardId);
+test(
+  'clicking all the containers marks all of them as crossed',
+  withPage,
+  async (t, page) => {
+    const cardId = await getOneBingoCard(getBaseAppUrl());
+    const cardUrl = getUrlForCard(cardId);
 
-//     await page.goto(cardUrl);
+    await page.goto(cardUrl);
 
-//     const numberContainers = await page.$$(
-//       '#app-root .content .card .number-container:not(.is-header)'
-//     );
+    const points = [...Array(5).keys()]
+      .map((e) => e + 1)
+      .flatMap((c) =>
+        [...Array(5).keys()].map((r) => r + 1).map((r) => [r, c])
+      );
 
-//     await Promise.all(numberContainers.map((c) => c.click()));
+    for (const [r, c] of points) {
+      await page.click(
+        `#app-root .content .card .number-container[data-row="${r}"][data-column="${c}"]:not(.is-header)`
+      );
+    }
 
-//     const numberCrosses = await page.$$(
-//       '#app-root .content .card .number-container:not(.is-header) .number-cross.visible'
-//     );
+    const numberCrosses = await page.$$(
+      '#app-root .content .card .number-container:not(.is-header) .number-cross.visible'
+    );
 
-//     t.is(numberCrosses.length, 25);
-//   }
-// );
+    t.is(numberCrosses.length, 25);
+  }
+);
