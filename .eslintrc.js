f547d1d1ec5e0cdfe74c1b39054f1ea414e56c69
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2023-09-15 23:06:32 -0400

    started migration over to fuckin eslint, tslint test now sweet prince

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended-type-checked',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  parserOptions: {
+    project: true,
+    tsconfigRootDir: __dirname,
+  },
+  rules: {
+    '@typescript-eslint/restrict-template-expressions': 'off',
+    '@typescript-eslint/no-base-to-string': 'off',
+    '@typescript-eslint/no-floating-promises': [
+      'error',
+      {
+        ignoreVoid: true,
+      },
+    ],
+  },
+  root: true,
+};
